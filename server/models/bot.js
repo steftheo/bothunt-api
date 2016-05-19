81cc3f5353bb331e5a1abdb56a15d73d4b6aeccf
Stefan Theofilos <StefanTheofilos@Stefans-MacBook-Pro.local>
2016-05-18 22:27:48 -0500

    Bot should save it's user

--- server/models/bot.js
+++ server/models/bot.js
@@ -1,2 +1,13 @@
 module.exports = function(Bot) {
+  Bot.beforeRemote('create', function(context, user, next) {
+    var req = context.req;
+
+    req.body.data.relationships['end-user'] = req.body.data.relationships['end-user'] || {};
+
+    req.body.data.relationships['end-user'].data = {
+      id: req.accessToken.userId,
+    };
+
+    next();
+  });
 };
